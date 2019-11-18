49f5f5798b2a76599eedb7c8a8e39a07395b1d68
UnstoppableNigeria <brandinfoot@gmail.com>
2019-11-18 11:10:08 -0500

    Initial Creation
    ...and He said, "Let there be files"

---
+++ desktop/src/com/bejk/main/desktop/CaveGameDesktop.java
@@ -0,0 +1,12 @@
+package com.bejk.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bejk.main.MainGame;
+
+public class CaveGameDesktop {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
